1616f9d0ae251bf354d4705cfc34d837f1e23699
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-02 23:09:42 +0530

    First commit with basic setup of frontend and the Navbar on home page

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { use } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourite from './pages/Favourite'
+import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
+
+const App = () => {
+
+  const isAdminroute = useLocation().pathname.startsWith('/admin')
+  return (
+    <>
+    <Toaster/>
+    { !isAdminroute && <Navbar/> }
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/movies' element={<Movies/>}/>
+      <Route path='/movies/:id' element={<MovieDetails/>}/>
+      <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+      <Route path='/my-bookings' element={<MyBookings/>}/>
+      <Route path='/favourite' element={<Favourite/>}/>
+
+    </Routes>
+        { !isAdminroute && <Footer/> }
+
+    </>
+  )
+}
+
+export default App
