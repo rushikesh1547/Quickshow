3c554c0228f218785c06b93f08992409520f9d36
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-09 22:37:12 +0530

    enhance MyBookings page with payment button and ticket details display and the user interface pages UI is completed

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -39,7 +39,18 @@ const MyBookings = () => {
               <p className='text-gray-400 text-sm mt-auto'>{dateFormat(item.show.showDateTime)}</p>
             </div>
           </div>
+            <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
+              <div className='flex items-center gap-4'>
+                <p className='text-2xl font-semibold mb-3 '>{currency}{item.amount}</p>
+                {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</button>}
+              </div>
+              <div className='text-sm'>
+                <p><span className='text-gray-400'>Total Tickets:</span> {item.bookedSeats.length}</p>
+                <p><span className='text-gray-400'>Seat Number:</span> {item.bookedSeats.join(', ')}</p>
 
+
+              </div>
+            </div>
         </div>
       ))}
     </div>
