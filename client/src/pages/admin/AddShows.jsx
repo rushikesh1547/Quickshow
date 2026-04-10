076c8c420a001bfa6303345d7499499529eb9bd0
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-10 23:17:01 +0530

    add admin panel components and routing for shows and bookings management and created admin navbar

---
+++ client/src/pages/admin/AddShows.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const addShows = () => {
+  return (
+    <div>addShows</div>
+  )
+}
+
+export default addShows
---
+++ client/src/pages/admin/ListShows.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListShows = () => {
+  return (
+    <div>listShows</div>
+  )
+}
+
+export default ListShows
