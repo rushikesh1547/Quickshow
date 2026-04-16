ee0c8905081990f347e810d20e9c2cfa90195977
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-16 23:00:16 +0530

    integrate Inngest for user data synchronization and create User model

---
+++ server/inngest/index.js
@@ -0,0 +1,28 @@
+import {Inngest} from 'inngest'
+import User from '../models/User.js';
+
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+//Inngest function to save user data to database
+const syncUserCreation = inngest.createFunction(
+    {
+        id: "sync-user-from-clerk",
+        triggers: { event: 'clerk/user.created' },
+    },
+
+    async ({event})=> {
+        const {id, first_name,last_name , email_addresses, image_url} = event.data 
+        const userData = {
+            _id: id, 
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url,
+        }   
+        await User.create(userData)
+    }
+)
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUserCreation];
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import  'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
 
 const app = express();
 const port = 3000;
@@ -19,5 +22,7 @@ app.use(clerkMiddleware());
 app.get('/', (req, res) => {
   res.send('Server is live');
 })
+
+app.use('/api/inngest', serve({ client: inngest, functions }) )
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
