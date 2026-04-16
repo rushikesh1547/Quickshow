4c9b52c7d51c63de6be64bb97166bb6f4a5778c9
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-16 22:17:56 +0530

    add Clerk authentication and Inngest dependency
    - Added @clerk/express for authentication middleware
    - Integrated clerkMiddleware into the Express app
    - Included Inngest as a new dependency
    - Established MongoDB connection on server startup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import  'dotenv/config';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const port = 3000;
+
+await connectDB();
 
 
 //middleware 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 //API routes
 app.get('/', (req, res) => {
