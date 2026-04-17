51198fd4633cc5af3bd2ad51a854efe453bd8859
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-17 22:54:51 +0530

    add getNowPlayingMovies function to fetch current movies from TMDB API

---
+++ server/controllers/showController.js
@@ -0,0 +1,17 @@
+
+
+export const getNowPlayingMovies = async (req, res) => {
+    try {
+        const { data } =  await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
+            headers: {
+                Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+            }
+        })
+
+        const movies = data.results;
+        res.json({success: true, movies: movies})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: error.message})
+    }
+}
