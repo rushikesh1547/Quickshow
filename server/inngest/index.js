e6a34d3f0d29aed54925cde540029b5850346994
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-17 18:47:05 +0530

    add user deletion and update functions for Clerk integration

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -24,5 +24,36 @@ const syncUserCreation = inngest.createFunction(
     }
 )
 
+//Inngest Function to delete user from database 
+const syncUserDeletion = inngest.createFunction(
+    {
+        id: "delete-user-with-clerk",
+        triggers: { event: 'clerk/user.deleted' },
+    },
+    async ({event})=> {
+    const {id} = event.data
+    await User.findByIdAndDelete(id)
+    }
+)
+
+//Inngest Function to update user in database 
+const syncUserUpdation = inngest.createFunction(
+    {
+        id: "update-user-from-clerk",
+        triggers: { event: 'clerk/user.updated' },
+    },
+    async ({event})=> {
+        const {id, first_name,last_name , email_addresses, image_url} = event.data 
+        const userData = {
+            _id: id, 
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url,
+        }   
+        await User.findByIdAndUpdate(id, userData)
+
+    }
+)
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
