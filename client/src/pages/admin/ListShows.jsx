6740a43950024fa48ac7b17e14c7eda18ff516a8
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-21 22:25:40 +0530

    add: fetch shows from API in ListShows component and handle loading state

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -4,34 +4,37 @@ import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
 import { useEffect } from 'react';
 import { dateFormat } from '../../lib/dateFormat';
+import { useAppContext } from '../../context/AppContext';
 
 const ListShows = () => {
+
   const currency = import.meta.env.VITE_CURRENCY
+
+  const {axios, getToken, user, } = useAppContext()
+  
   const [shows, setShows] = useState([])
   const [loading ,setLoading ] = useState(true);
 
 
   const getAllShows = async () => {
     try {
-      setShows([{
-        movie: dummyShowsData[0],
-        showDateTime: "2025-06-30T02:30:00.000Z",
-        showPrice: 59,
-        occupiedSeats: {
-          A1: "user_1",
-          B1: "user_2",
-          C1: "user_3"
+      const {data} = await axios.get('/api/admin/all-shows', {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`
         }
-      }]);
+      })
+      setShows(data.shows)
       setLoading(false);
     } catch (error) {
       console.error(error)
+      setLoading(false);
     }
   }
 
   useEffect(() => {
+if(user){
     getAllShows();
-  }, [])
+}  }, [user])
 
   return !loading ? (
     <>
@@ -47,7 +50,7 @@ const ListShows = () => {
             </tr>
           </thead>
           <tbody className='text-sm font-light'>
-            {shows.map((show, index) => (
+            {(shows || []).map((show, index) => (
               <tr key={index} className='border-b border-primary/10 bg-primary/5 even:bg-primary/10'>
                 <td className='p-2 pl-5 min-w-45'>{show.movie.title}</td>
                 <td className='p-2'>{dateFormat(show.showDateTime)}</td>
