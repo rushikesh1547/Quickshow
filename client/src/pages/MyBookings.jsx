d03a44e496d344ba8fe4f1a34ba2380bdd3388b4
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-24 00:16:18 +0530

    refactor: replace Pay Now button with Link for payment navigation in MyBookings

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -6,6 +6,7 @@ import { dummyBookingData } from '../assets/assets'
 import { dateFormat } from '../lib/dateFormat'
 import { useAppContext } from '../context/AppContext'
 import { toast } from 'react-hot-toast'
+import { Link } from 'react-router-dom'
 
 const MyBookings = () => {
 
@@ -69,14 +70,13 @@ const MyBookings = () => {
             <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
               <div className='flex items-center gap-4'>
                 <p className='text-2xl font-semibold mb-3 '>{currency}{item.amount}</p>
-                {!item.isPaid && (
-                  <button
-                    onClick={() => handlePayNow(item.paymentLink)}
+                {!item.isPaid && 
+                  <Link to={item.paymentLink}
                     className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'
                   >
                     Pay Now
-                  </button>
-                )}
+                  </Link>
+                }
               </div>
               <div className='text-sm'>
                 <p><span className='text-gray-400'>Total Tickets:</span> {item.bookedSeats.length}</p>
