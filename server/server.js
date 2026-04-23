bd972fe64e0f6d19d66cecc7f69c21569348f165
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-23 23:53:51 +0530

    feat: add Stripe webhooks route for payment processing

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import { stripeWebhooks } from './controllers/stripeWebhooks.js';
+
 
 
 const app = express();
@@ -16,6 +18,8 @@ const port = 3000;
 
 await connectDB();
 
+//stripe webhooks route
+app.use('/api/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
 
 //middleware 
 app.use(express.json());
