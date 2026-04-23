e7789c1b24836cac34a7098245c7b9af1a2c0eaa
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-23 23:51:23 +0530

    feat: implement Stripe webhooks for payment intent handling

---
+++ server/controllers/stripeWebhooks.js
@@ -0,0 +1,45 @@
+import stripe from 'stripe';
+import Booking from '../models/Booking.js';
+
+export const stripeWebhooks = async (requset, response) => {
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+    const sig = requset.headers['stripe-signature'];
+
+    let event;
+    try {
+        event = stripeInstance.webhooks.constructEvent(requset.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (error) {
+        return response.status(400).send(`Webhook Error: ${error.message}`);
+    }
+
+    try {
+        switch (event.type) {
+            case "payment_intent.succeeded":
+                // Handle successful payment intent
+                {
+                    const paymentIntent = event.data.object;
+                    const sessionList = await stripeInstance.checkout.sessions.list({
+                        payment_intent:paymentIntent.id,
+                    });
+                    const session = sessionList.data[0];
+                    const {bookingId} = session.metadata;
+
+                    await Booking.findByIdAndUpdate(bookingId, {
+                        isPaid: true,
+                        paymentLink: ''
+                    })
+                     break;
+
+                }
+                
+        
+            default:
+                console.log(`Unhandled event type ${event.type}`);
+                break;
+        }
+        response.json({ received: true });
+    } catch (err) {
+        console.error(`Webhook processing error: `, err);
+        response.status(500).json({ error: 'Internal server error' });
+    }
+}
