860bf6428bf45383a306cf00b7f1428ee80dce50
rushikesh1547 <rushikeshchandekar1547@gmail.com>
2026-04-24 22:38:43 +0530

    send confirmation email to user after successful payment in stripe webhooks

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -1,5 +1,6 @@
 import stripe from 'stripe';
 import Booking from '../models/Booking.js';
+import { inngest } from '../inngest/index.js';
 
 export const stripeWebhooks = async (requset, response) => {
     const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
@@ -28,6 +29,13 @@ export const stripeWebhooks = async (requset, response) => {
                         isPaid: true,
                         paymentLink: ''
                     })
+
+                    //Send confirmation email to user after successful payment
+                    await inngest.send({
+                        name: "app/show.booked",
+                        data: {bookingId}
+                    })
+
                      break;
 
                 }
